58863a1d1be290039776ccf40cc477e7089607bd
BandsThoBaby <32070633+BandsThoBaby@users.noreply.github.com>
2023-04-26 21:06:47 +0100

    Add files via upload

---
+++ FC_name_generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Sawadee Krup!")
+#2. Ask the user for the city that they grew up in.
+province = input("What province are you from?\n")
+#3. Ask for their favourite verb.
+verb = input("What's your favourite verb?\n")
+#4. Combine the name of their city and verb and show them their Football Team name.
+football_club_name = province + " " + verb + " " + "Football Club"
+#5. Make sure the input cursor shows on a new line:
+print("Your Football Club name is " + football_club_name)
+
